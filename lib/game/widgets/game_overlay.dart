7e4e2f92a0332ad06c323f26e6663731daf57738
Joshua Fernandes <fernandesjoshua38@gmail.com>
2024-07-28 18:41:23 +0530

    updated levels

--- lib/game/widgets/game_overlay.dart
+++ lib/game/widgets/game_overlay.dart
@@ -27,91 +27,118 @@ class GameOverlayState extends State<GameOverlay> {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
-      child: SafeArea (
-        child: Stack(
+      child: SafeArea(
+        child: Column(
           children: [
-            Positioned(
-              top: 30,
-              left: 30,
-              child: ScoreDisplay(game: widget.game),
-            ),
-            Positioned(
-              top: 30,
-              right: 30,
-              child: ElevatedButton(
-                child: isPaused
-                    ? const Icon(
-                        Icons.play_arrow,
-                        size: 48,
-                      )
-                    : const Icon(
-                        Icons.pause,
-                        size: 48,
+            Stack(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30, left: 30),
+                      child: ScoreDisplay(game: widget.game),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30, right: 30),
+                      child: ElevatedButton(
+                        child: isPaused
+                            ? const Icon(
+                                Icons.play_arrow,
+                                size: 48,
+                              )
+                            : const Icon(
+                                Icons.pause,
+                                size: 48,
+                              ),
+                        onPressed: () {
+                          (widget.game as DoodleDash).togglePauseState();
+                          setState(
+                            () {
+                              isPaused = !isPaused;
+                            },
+                          );
+                        },
                       ),
-                onPressed: () {
-                  (widget.game as DoodleDash).togglePauseState();
-                  setState(
-                    () {
-                      isPaused = !isPaused;
-                    },
-                  );
-                },
-              ),
+                    ),
+                  ],
+                ),
+                if (isPaused)
+                  Positioned(
+                    top: MediaQuery.of(context).size.height / 2 - 72.0,
+                    right: MediaQuery.of(context).size.width / 2 - 72.0,
+                    child: const Icon(
+                      Icons.pause_circle,
+                      size: 144.0,
+                      color: Colors.black12,
+                    ),
+                  ),
+              ],
             ),
             if (isMobile)
-              Positioned(
-                bottom: MediaQuery.of(context).size.height / 4,
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 24),
-                        child: GestureDetector(
-                          onTapDown: (details) {
-                            (widget.game as DoodleDash).player.moveLeft();
-                          },
-                          onTapUp: (details) {
-                            (widget.game as DoodleDash).player.resetDirection();
-                          },
-                          child: Material(
-                            color: Colors.transparent,
-                            elevation: 3.0,
-                            shadowColor: Theme.of(context).colorScheme.background,
-                            child: const Icon(Icons.arrow_left, size: 64),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: GestureDetector(
+                        onTapDown: (details) {
+                          (widget.game as DoodleDash).player.moveLeft();
+                        },
+                        onTapUp: (details) {
+                          (widget.game as DoodleDash).player.resetDirection();
+                        },
+                        child: Container(
+                          color: Colors.transparent,
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 24),
+                              child: Material(
+                                color: Colors.transparent,
+                                elevation: 3.0,
+                                shadowColor:
+                                    Theme.of(context).colorScheme.background,
+                                child: const Icon(Icons.arrow_left, size: 64),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 24),
-                        child: GestureDetector(
-                          onTapDown: (details) {
-                            (widget.game as DoodleDash).player.moveRight();
-                          },
-                          onTapUp: (details) {
-                            (widget.game as DoodleDash).player.resetDirection();
-                          },
-                          child: Material(
-                            color: Colors.transparent,
-                            elevation: 3.0,
-                            shadowColor: Theme.of(context).colorScheme.background,
-                            child: const Icon(Icons.arrow_right, size: 64),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                          color: Colors.transparent), // Inactive center area
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: GestureDetector(
+                        onTapDown: (details) {
+                          (widget.game as DoodleDash).player.moveRight();
+                        },
+                        onTapUp: (details) {
+                          (widget.game as DoodleDash).player.resetDirection();
+                        },
+                        child: Container(
+                          color: Colors.transparent,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 24),
+                              child: Material(
+                                color: Colors.transparent,
+                                elevation: 3.0,
+                                shadowColor:
+                                    Theme.of(context).colorScheme.background,
+                                child: const Icon(Icons.arrow_right, size: 64),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-            if (isPaused)
-              Positioned(
-                top: MediaQuery.of(context).size.height / 2 - 72.0,
-                right: MediaQuery.of(context).size.width / 2 - 72.0,
-                child: const Icon(
-                  Icons.pause_circle,
-                  size: 144.0,
-                  color: Colors.black12,
+                    ),
+                  ],
                 ),
               ),
           ],
